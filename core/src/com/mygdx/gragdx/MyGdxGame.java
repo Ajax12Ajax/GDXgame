83eb7e6e443f43caf7e18cee3b0b8ce77a13da5a
Ajax12Ajax <68964892+Ajax12Ajax@users.noreply.github.com>
2020-08-15 14:01:37 +0200

    graGDX | Learning & repair

--- core/src/com/mygdx/gragdx/MyGdxGame.java
+++ core/src/com/mygdx/gragdx/MyGdxGame.java
@@ -4,26 +4,47 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
+	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera(1, h/w);
+
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		batch = new SpriteBatch();
-		img = new Texture("alternator.jpg");
+		img = new Texture("badlogic.jpg");
+		img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+		TextureRegion region = new TextureRegion(img, 0, 0, 256, 256);
+
+
+		sprite = new Sprite(region);
+		sprite.setSize(0.5f, 0.5f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -0.19f);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 	
