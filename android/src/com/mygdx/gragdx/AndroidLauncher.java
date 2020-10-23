580d7cfdfbf788c7b2f8dc7d24989969f852e761
Ajax12Ajax <68964892+Ajax12Ajax@users.noreply.github.com>
2020-10-23 20:04:03 +0200

    GDXgame | Creation

--- android/src/com/mygdx/gragdx/AndroidLauncher.java
+++ android/src/com/mygdx/gragdx/AndroidLauncher.java
@@ -1,18 +1,25 @@
 package com.mygdx.gragdx;
 
 import android.os.Bundle;
-
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.Gdx;
+import android.view.View;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.gragdx.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		View decorView = getWindow().getDecorView();
+
+
+		decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 		initialize(new MyGdxGame(), config);
+
 	}
 }
